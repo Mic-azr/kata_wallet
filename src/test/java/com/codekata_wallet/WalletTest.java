9c274ff34f9dbb061294d6a9a183d868c10f9d76
Mic-azr <michael.cummings1997@gmail.com>
2025-04-07 18:33:22 -0400

    First test for the Wallet class

--- src/test/java/com/codekata_wallet/WalletTest.java
+++ src/test/java/com/codekata_wallet/WalletTest.java
@@ -5,6 +5,7 @@
 
 package com.codekata_wallet;
 
+import java.util.LinkedList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,5 +14,23 @@ import static org.junit.Assert.*;
  * @author Michael
  */
 public class WalletTest {
-    // TODO add test methods here.
+    @Test
+    public void testWalletConstructorAndGetters() {
+        LinkedList<Stock> expectedStocks = new LinkedList<>();
+        LinkedList<Stock> actualStocks = new LinkedList<>();
+
+        Stock stock1 = new Stock(100, "USD");
+        Stock stock2 = new Stock(50, "EUR");
+
+        expectedStocks.add(stock1);
+        expectedStocks.add(stock2);
+        actualStocks.add(stock1);
+        actualStocks.add(stock2);
+
+        Wallet wallet = new Wallet(actualStocks);
+        assertEquals("Stocks should equal" + expectedStocks, expectedStocks, wallet.getStocks()); //Wallet should contain a LinkedList of Stocks equal to actualStocks
+    }
+
+    // TODO: testGetTotalValue
+    // @Test
 }
