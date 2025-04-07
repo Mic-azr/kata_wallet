ed63e8c0d42dc49a130413e8d9e72d436e19d693
Mic-azr <michael.cummings1997@gmail.com>
2025-04-07 18:18:32 -0400

    Working on Wallet class, first implementation of getTotalValue() depends on CurrencyCoverter's getExchangeRate() method which is not written yet

--- src/main/java/com/codekata_wallet/Wallet.java
+++ src/main/java/com/codekata_wallet/Wallet.java
@@ -1,5 +1,35 @@
 package com.codekata_wallet;
 
+import java.util.LinkedList;
+
 public class Wallet {
-    // This isn't right... looks a lot like the Stock class
+    private LinkedList<Stock> stocks = new LinkedList<>(); 
+
+    public Wallet (LinkedList<Stock> stocks) {
+        this.stocks = stocks;
+    }
+    public void addStock(Stock stock) {
+        this.stocks.add(stock);
+    }
+    public void removeStock(Stock stock) {
+        this.stocks.remove(stock);
+    }
+    public LinkedList<Stock> getStocks() {
+        return this.stocks;
+    }
+    public double getTotalValue(String targetCurrency, CurrencyConverter currencyConverter) {
+        double totalValue = 0.0;
+        for (Stock stock : stocks) {
+            double exchangeRate = currencyConverter.getExchangeRate(stock.getStockType(), targetCurrency);
+            totalValue += stock.getQuantity() * exchangeRate;
+        }
+        return totalValue;
+    }
+    
+    // TODO: Create a printWalletContents method that would output the contents of the wallet in a readable format.
+    // Ex:
+    // """ WALLET CONTENTS """
+    // 100 X USD
+    // 50 X EUR
+    // 250 X GBP
 }
