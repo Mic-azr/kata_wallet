761a971cb464b5e3c98ae7c73dfcc852e8cfa133
Mic-azr <michael.cummings1997@gmail.com>
2025-04-16 13:54:46 -0400

    Beginning refactor of Wallet

--- src/main/java/com/codekata_wallet/Wallet.java
+++ src/main/java/com/codekata_wallet/Wallet.java
@@ -1,30 +1,24 @@
 package com.codekata_wallet;
 
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Wallet {
-    private LinkedList<Stock> stocks = new LinkedList<>(); 
+    private Map<String, Stock> stocks;
 
-    public Wallet (LinkedList<Stock> stocks) {
-        this.stocks = stocks;
+    public Wallet() {
+        this.stocks = new HashMap<>();
     }
-    public void addStock(Stock stock) {
-        this.stocks.add(stock);
-    }
-    public void removeStock(Stock stock) {
-        this.stocks.remove(stock);
-    }
-    public LinkedList<Stock> getStocks() {
-        return this.stocks;
-    }
-    public double getTotalValue(String targetCurrency, CurrencyConverter currencyConverter) {
-        double totalValue = 0.0;
-        for (Stock stock : stocks) {
-            double exchangeRate = currencyConverter.getExchangeRate(stock.getStockType(), targetCurrency);
-            totalValue += stock.getQuantity() * exchangeRate;
-        }
-        return totalValue;
-    }
+
+    // TODO:
+    // public void addStock(Stock stock) {}
+
+    // TODO:
+    // public Stock getStock(String stockType) {}
+
+    // TODO:
+    // public int getWalletValue() {}
+
     
     // TODO: Create a printWalletContents method that would output the contents of the wallet in a readable format.
     // Ex:
--- src/test/java/com/codekata_wallet/WalletTest.java
+++ src/test/java/com/codekata_wallet/WalletTest.java
@@ -15,22 +15,13 @@ import static org.junit.Assert.*;
  */
 public class WalletTest {
     @Test
-    public void testWalletConstructorAndGetters() {
-        LinkedList<Stock> expectedStocks = new LinkedList<>();
-        LinkedList<Stock> actualStocks = new LinkedList<>();
-
-        Stock stock1 = new Stock(100, "USD");
-        Stock stock2 = new Stock(50, "EUR");
-
-        expectedStocks.add(stock1);
-        expectedStocks.add(stock2);
-        actualStocks.add(stock1);
-        actualStocks.add(stock2);
-
-        Wallet wallet = new Wallet(actualStocks);
-        assertEquals("Stocks should equal" + expectedStocks, expectedStocks, wallet.getStocks()); //Wallet should contain a LinkedList of Stocks equal to actualStocks
-    }
+    // public void testWalletConstructorAndGetters() {
+    //    @TODO: Refactor test for a Wallet which uses the Dictionary data type
+    // }
 
     // TODO: testGetTotalValue
+    // @Test
+    //
+    // TODO: testprintWalletContents
     // @Test
 }
